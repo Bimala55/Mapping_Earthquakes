1dc4901ef275f3e32111dd2d91f14798103acc30
Bimala Basnet <bbimala85@gmail.com>
2021-05-11 08:16:52 -0400

    Added Oneachfeature function

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -31,32 +31,11 @@ let sanFranAirport =
 
 // Grabbing our GeoJSON data.
 L.geoJson(sanFranAirport, {
-  // We turn each feature into a marker on the map.
-  pointToLayer: function (feature, latling) {
-    console.log(feature);
-    return L.marker(latling)
-     .bindPopup("<h2>" + feature.properties.name + "</h2><hr> <h3>  "+ feature.properties.city+","+ feature.properties.country+ "</h3>");
-
+  onEachFeature: function (feature, layer) {
+    console.log(layer);
+    layer.bindPopup();
   }
-
 }).addTo(map);
-
-// Coordinates for each point to be used in the polyline.
-//let line = [
-// [37.6213, -122.3790],
-//[30.1975, -97.6664],
-//[43.6777, -79.6248],
-//[40.6413, -73.7781]
-//];
-
-// Create a polyline using the line coordinates and make the line black.
-//L.polyline(line, {
-// color: "blue",
-//dashArray: 10,
-//weight: 4,
-///opacity: 0.5
-//}).addTo(map);
-
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
