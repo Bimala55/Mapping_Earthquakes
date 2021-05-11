cbfcb05289d60da344781df2cc084d1813e6b3f0
Bimala Basnet <bbimala85@gmail.com>
2021-05-10 20:44:59 -0400

    Added map multiple lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -5,17 +5,17 @@ console.log("working");
 let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
 // Coordinates for each point to be used in the polyline.
-// Coordinates for each point to be used in the line.
 let line = [
     [33.9416, -118.4085],
-    [37.6213, -122.3790]
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
   ];
 
-// Create a polyline using the line coordinates and make the line red.
+// Create a polyline using the line coordinates and make the line black.
 L.polyline(line, {
-    color: "red"
-  }).addTo(map);
-
+    color: "yellow"
+ }).addTo(map);
 
 // Get data from cities.js
 //let cityData = cities;
